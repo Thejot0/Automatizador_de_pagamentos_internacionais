2378384cfd614cc7468e99275f05877634eda382
the_jota <jeremiassantana50@gmail.com>
2026-08-28 14:52:40 -0300

    Renomeei o nome da pasta onde iria armazenar os loggins

--- adm_logger/loggin.py
+++ adm_logger/loggin.py
@@ -4,7 +4,7 @@ logging.basicConfig(
     level=logging.INFO,
     format= "%(asctime)s[%(levelname)s]: %(message)s",
     datefmt= "%d/%m/%Y - [%H:%M:%S]",
-    filename='pasta_log.log',
+    filename='.pasta_log.log',
     encoding='utf-8',
     force = True
 
